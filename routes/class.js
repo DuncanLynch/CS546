e31a2151dadd007e7ecfaacadf1363a6ef2a0fa8
Lewis Goldenberg <lewisgoldenberg.0@gmail.com>
2025-05-07 02:12:45 -0400

    made /class/course_code usable for now, see my guide on how to use it in comments

--- routes/class.js
+++ routes/class.js
@@ -49,16 +49,32 @@ router
         return res.status(500).send("500: " + e)
     }
 })
-// router
-// .route('/:course_code')
-// .get(async (req, res) => {
-//     try{
-//         const foundClass = await classData.getClassByCourseCode(req.params.id)
-//         return res.status(200).render('classpage', {class: foundClass})
-//     }catch (e){
-//         if(e === "Class not found") return res.status(404).send("404: "+ e)
-//         return res.status(500).send("500: " + e)
-//     }
+router
+.route('/:course_code')
+.get(async (req, res) => {
+    try{
+        //const foundClass = await classData.getClassByCourseCode(req.params.id) commented until getclassbycoursecode is made
+
+        //CHANGE THE VALUES OF THIS OBJECT BELOW HOW YOU LIKE, IF YOU NEED MORE DATA PASSED INTO YOUR COURSE PAGE JUST ASK ME, FOR NOW I AM JUST PASSING A COURSE OBJECT
+        //THIS TEST OBJECT DOES NOT HAVE A _id PARAMETER, IN THE REAL DEAL IT WILL HAVE ONE, FOR NOW JUST PRETEND LIKE IT HAS ONE
+        const foundClass = {
+            course_code: "CS 546",
+            course_name: "Web Programming",
+            course_description: "This course will provide students with a first strong approach of internet programming. It will give the basic knowledge on how the Internet works and how to create advanced web sites by the use of script languages, after learning the basics of HTML. The course will teach the students how to create a complex global site through the creation of individual working modules, giving them the skills required in any business such as proper team work and coordination between groups.",
+            typically_offered: "Fall Semester Spring Semester",
+            prerequisites: "CS 182 or CS 385 or CS 590 (Completed or In-Process)",
+            class_total_rating: 1,
+            class_total_difficulty: 1 ,
+            class_total_quality: 1,
+            professors: [],
+            reviews: []
+        }
+        //CHANGE CLASSPAGE TO THE NAME OF THE CLASS PAGE HANDLEBARS FILE
+        return res.status(200).render('classpage', {class: foundClass})
+    }catch (e){
+        //if(e === "Class not found") return res.status(404).send("404: "+ e)
+        return res.status(500).send("500: " + e)
+    }
     
-// })
+})
 export default router
