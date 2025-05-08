b313b772e04418a09fedeb1c0dac5d32fd1a8a7f
Gabriel Castillo <gabe@Gabriels-Air.lan>
2025-05-08 15:56:03 -0400

    middlware, gonna use ajax, and scuffed handlebars

--- data/users.js
+++ data/users.js
@@ -1,14 +1,76 @@
-//Imports
 
-//Data Functions:
-export async function createUser(user_name, password, email) {}
+// Imports
+import { ObjectId } from "mongodb";
+import bcrypt from "bcrypt";
+import { users } from "../mongodb/mongoCollections";
 
-export async function getUserById(id) {}
+// Constants
+const SALT_ROUNDS = 10;
 
-export async function getAllUsers() {}
+// Data Functions:
+export async function createUser(user_name, password, email) {
+    //Validate
 
-export async function deleteUser(id) {}
+    const userCollection = await users();
 
-export async function addReview(user_id, review_object) {}
+    const existingUser = await userCollection.findOne({ user_name });
+    if (existingUser) throw new Error("Username already exists.");
 
-export async function validateUser(user_name, password) {}
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+
+    const newUser = {
+        user_name,
+        hashed_password: hashedPassword,
+        email,
+        reviews: [] // optional field to hold reviews
+    };
+
+    const insertResult = await userCollection.insertOne(newUser);
+    if (!insertResult.acknowledged) throw new Error("Failed to create user.");
+
+    return { _id: insertResult.insertedId, user_name, email };
+}
+
+export async function getUserByName(user_name) {
+    //Validate
+    const userCollection = await users();
+    const user = await userCollection.findOne({ user_name });
+    if (!user) throw new Error("User not found.");
+    return user;
+}
+
+export async function getAllUsers() {
+    const userCollection = await users();
+    return await userCollection.find({}).toArray();
+}
+
+export async function deleteUser(id) {
+    //Validate
+    const userCollection = await users();
+    const result = await userCollection.deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) throw new Error("User not found or not deleted.");
+    return true;
+}
+
+export async function addReview(user_id, review_object) {
+    //Validate
+    const userCollection = await users();
+    const result = await userCollection.updateOne(
+        { _id: new ObjectId(user_id) },
+        { $push: { reviews: review_object } }
+    );
+    if (result.modifiedCount === 0) throw new Error("Failed to add review.");
+    return true;
+}
+
+export async function validateUser(user_name, password) {
+    // Validate
+    const userCollection = await users();
+    const user = await userCollection.findOne({ user_name });
+    if (!user) throw new Error("Invalid username or password.");
+
+    const match = await bcrypt.compare(password, user.hashed_password);
+    if (!match) throw new Error("Invalid username or password.");
+
+    return { _id: user._id, user_name: user.user_name, email: user.email };
+}
---
+++ routes/reviews.js
@@ -0,0 +1,85 @@
+import express from 'express';
+import * as classData from '../data/classes.js'
+import * as userData from '../data/users.js'
+const router = express.Router();
+router
+.route('/:classId')
+.get(async (req, res) => {
+    const classId = xss(req.params.id)
+    try {
+        const reviews = await classData.getClassById(classId).reviews
+        return res.status(200).json(reviews);
+    } catch (e) {
+    // Something went wrong with the server!
+        return res.status(404).send(e);
+    }
+})
+.post(async (req, res) => {
+    const classId = xss(req.params.id)
+    const course_code = xss(req.body.course_code)
+    const professor_id = xss(req.body.professor_id)
+    const review_title = xss(req.body.review_title)
+    const reviewer_id = xss(req.body.reviewer_id)
+    const review_date = xss(req.body.review_date) 
+    const review_contents = xss(req.body.review_contents) 
+    const review_quality_rating = xss(req.body.review_quality_rating)
+    const review_difficulty_rating = xss(req.body.review_difficulty_rating)
+    const review_total_rating = xss(req.body.review_total_rating)
+    const user_name = xss(req.session.user_name)
+    try{
+        const newReview = await classData.addReview(classId, course_code, professor_id, review_title, reviewer_id, review_date, review_contents, review_quality_rating, review_difficulty_rating, review_total_rating)
+        await userData.addReview(user_name, newReview) //awaiting on this function, update param upon duncan push
+        return res.status(200).send(newReview)
+
+    }catch(e){
+        return res.status(404).send(e);
+    }
+})
+router
+.route('/review/:id')
+.get(async (req, res) => {
+    const id = xss(req.params.id)
+    try{
+        const classList = await classData.getAllClasses()
+        for(let i = 0; i<classList.length; i++){
+            for(let j = 0; j<classList[i].reviews.length; j++){
+                if(classList[i].reviews[j]._id == id) return res.status(200).send(classList[i].reviews[j])
+            }
+        }
+        return res.status(404).send("404: Review not found")
+    }catch(e){
+        return res.status(500).send("500: " + e)
+    }
+})
+.delete(async (req, res) => {
+    const id = xss(req.params.id)
+    try{
+        const deletedReview = await classData.deleteReview(id) //waiting on this function
+        await userData.deleteReview(id) //waiting on this function
+        return res.status(200).send(deletedReview)
+    }catch(e){
+        return res.status(404).send("404: " + e)
+    }
+
+})
+.put(async (req, res) => {
+    const id = xss(req.params.id)
+    const course_code = xss(req.body.course_code)
+    const professor_id = xss(req.body.professor_id)
+    const review_title = xss(req.body.review_title)
+    const reviewer_id = xss(req.body.reviewer_id)
+    const review_date = xss(req.body.review_date)
+    const review_contents = xss(req.body.review_contents)
+    const likes = xss(req.body.likes)
+    const dislikes = xss(req.body.dislikes)
+    const review_quality_rating = xss(req.bodyreview_quality_rating)
+    const review_difficulty_rating = xss(req.body.review_difficulty_rating)
+    const review_total_rating = xss(req.body.review_total_rating)
+    try{
+        const updatedReview = await classData.updateReview(id, course_code, professor_id, review_title, reviewer_id, review_date, review_contents, likes, dislikes, review_quality_rating, review_difficulty_rating, review_total_rating)//waiting on this function
+        return res.status(200).send(updatedReview)
+    }catch (e){
+        return res.status(404).send("404: "+ e)
+    }
+})
+export default router
